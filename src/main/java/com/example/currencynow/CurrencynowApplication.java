80b18652225e67b1a8853c3a26c1fe69ed66ab9f
Erick <profissional.erick@gmail.com>
2025-10-18 13:17:39 -0300

    feat: initial spring project setup

---
+++ src/main/java/com/example/currencynow/CurrencynowApplication.java
@@ -0,0 +1,13 @@
+package com.example.currencynow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencynowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencynowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/currencynow/CurrencynowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencynow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencynowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
